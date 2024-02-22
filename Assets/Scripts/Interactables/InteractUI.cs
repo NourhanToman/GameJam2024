75e3031f3577cd4e34013076b398dc19576592b9
MohamedAshrafElSayed <86211471+MohamedAshrafElSayed@users.noreply.github.com>
2024-02-23 00:24:52 +0200

    Added Interaction Logic

--- Assets/Scripts/Interactables/InteractUI.cs
+++ Assets/Scripts/Interactables/InteractUI.cs
@@ -1,32 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
 using UnityEngine;
 
 public class InteractUI : MonoBehaviour
 {
     [SerializeField] private GameObject _containerGO;
-    [SerializeField] private PlayerInteraction _playerInteract;
+    [SerializeField] private TextMeshProUGUI _tooltipText;
 
-
-    private void Update()
-    {
-        if (_playerInteract.GetInteractableObject() != null)
-        {
-            Show();
-        }
-        else
-        {
-            Hide();
-        }
-    }
-
-    private void Show()
+    public void Show()
     {
         _containerGO.SetActive(true);
     }
 
-    private void Hide()
+    public void Hide()
     {
         _containerGO.SetActive(false);
+        _tooltipText.SetText("");
+    }
+
+    public void SetTooltip(string tooltip)
+    {
+        _tooltipText.SetText(tooltip);
     }
 }
