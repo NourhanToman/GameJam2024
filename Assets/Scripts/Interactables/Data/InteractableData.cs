68d784a3103eb07fca96febdc9f3c68d40d162fd
MohamedAshrafElSayed <86211471+MohamedAshrafElSayed@users.noreply.github.com>
2024-02-22 22:14:11 +0200

    Added Interaction Data

---
+++ Assets/Scripts/Interactables/Data/InteractableData.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "InteractionDataSO", menuName = "Game Data/InteractionSO")]
+public class InteractableData : ScriptableObject
+{
+    private InteractableBase m_interactable;
+
+    public InteractableBase Interactable
+    {
+        get => m_interactable;
+        set => m_interactable = value;
+    }
+
+    public void Interact()
+    {
+        m_interactable.OnInteract();
+        ResetData();
+    }
+
+    public bool IsSameInteractable(InteractableBase _newInteractable) => m_interactable == _newInteractable;
+    public bool IsEmpty() => m_interactable == null;
+    public void ResetData() => m_interactable = null;
+}
