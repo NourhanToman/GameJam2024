5d8df7b38ac74b44fa963107fac83a1c3d197743
1Rooky <rokaiamedhat1@gmail.com>
2024-02-23 07:51:11 +0200

    little Updates

---
+++ Assets/Scripts/Puzzles/PasscodePuzzle.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PasscodePuzzle : MonoBehaviour
+{
+    [SerializeField] string passcodeAnswer;
+    public bool isSolved;
+    [SerializeField] InteractableObjText puzzleTxt;
+
+    public void StartPuzzle()
+    {
+
+    }
+}
