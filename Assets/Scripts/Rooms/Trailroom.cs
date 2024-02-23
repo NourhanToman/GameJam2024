a3da639e6a3450f4079ceb844f3040599b8cc4da
Nourhan Taman <147274340+NourhanToman@users.noreply.github.com>
2024-02-23 06:59:16 +0200

    Noorie Trail Updated

--- Assets/Scripts/Rooms/Trailroom.cs
+++ Assets/Scripts/Rooms/Trailroom.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class Trailroom : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+   
+    [SerializeField] int ReaperDistance= 10;
+    private bool isDialoge = false;
+    private float hitWaitTime = 20f;
     void Start()
     {
-        
+        StartCoroutine(StartHint());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+    
+        RaycastHit hit;
+
+
+        if (!isDialoge && Physics.Raycast(ray, out hit, ReaperDistance))
+        {
+            if (hit.collider.CompareTag("Reaper"))
+            {
+
+                TextManger.instance.PlayMessage(0);
+                isDialoge = true;
+
+            }
+        }
+
     }
+
+
+    private IEnumerator StartHint()
+    {
+        yield return new WaitForSeconds(hitWaitTime);
+        TextManger.instance.PlayMessage(1);
+
+    }
+
 }
