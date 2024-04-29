5ce62565504abb7bc7605c2adfa94da4bae78cd0
Nourhan Taman <147274340+NourhanToman@users.noreply.github.com>
2024-04-29 22:09:16 +0300

    PickUp Done

--- Assets/Scripts/Puzzles/PickUpLighter.cs
+++ Assets/Scripts/Puzzles/PickUpLighter.cs
@@ -6,27 +6,30 @@ public class PickUpLighter : MonoBehaviour
 {
     [SerializeField] private GameObject HandPos;
     [SerializeField] private LayerMask targetLayer;
-   
+    //private Vector3 handTrans;
+
+
+    private void Start()
+    {
+        //handTrans = new Vector3(0.0f,0.0f,0.0f);
+    }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) && (targetLayer & (1 << gameObject.layer)) != 0)
         {
-            //PickUpAction();
             PickUpPosition();
+            gameObject.layer = 0;
         }
     }
 
 
     void PickUpPosition()
     {   
-        transform.position = Vector3.zero; 
-        //transform.rotation = Quaternion.identity;
+       
         transform.SetParent(HandPos.transform);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
     }
 
-    /*void PickUpAction()
-    {
-
-    }*/
 }
